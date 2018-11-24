6fc6119b2b80129e0b35876fb7ffae130265945c
haotian <ritou11@gmail.com>
2018-11-24 22:56:56 +0800

    improve speed exp2

--- exp2.py
+++ exp2.py
@@ -5,53 +5,47 @@ from time import time
 from utils import csr_cosine_similarity
 from utils import HwGlobal as hg
 import numpy as np
+from scipy.sparse.linalg import norm
 
 t1 = time()
 
 with open('output/testMatrix.pkl', 'rb') as f:
-    testMatrix = pkl.load(f).tocoo()
+    testMatrix = pkl.load(f)
 with open('output/trainMatrix.pkl', 'rb') as f:
-    trainMatrix = pkl.load(f).tocsc()
+    trainMatrix = pkl.load(f)
 with open('output/simiMat.pkl', 'rb') as f:
     simiMat = pkl.load(f)
-
-def predict(i, j):
-    # tt1 = time()
-    traincol = trainMatrix.getcol(j)
-    upper = simiMat.getrow(i).dot(traincol)
-    # tt2 = time()
-    # 优化效果不明显，猜测原因为Python本身的缓存作用
-    lower = simiMat.getrow(i)[0, traincol.nonzero()[0]].sum()
-    # tt3 = time()
-    # print('up = %.2fms' % ((tt2 - tt1) * 1000))
-    # print('lw = %.2fms' % ((tt3 - tt2) * 1000))
-    return upper[0,0] / lower
 
 t2 = time()
 print('Load data: %.2fms' % ((t2 - t1) * 1000))
 
-rmse = 0
-predict_data = list()
-n = testMatrix.count_nonzero()
-alt = n // 100
-cnt = 0
-for (i,j,v) in zip(testMatrix.row, testMatrix.col, testMatrix.data):
-    p = predict(i, j)
-    cnt += 1
-    rmse += (p - v) ** 2
-    predict_data.append(p)
-    if cnt % alt == 0:
-        print('Process %.2f%%' % (cnt / n * 100))
-n = testMatrix.count_nonzero()
-rmse = np.sqrt(rmse / n)
+upper = simiMat * trainMatrix
 
 t3 = time()
-print('Predict test: %.2fms' % ((t3 - t2) * 1000))
+print('Calc upper: %.2fms' % ((t3 - t2) * 1000))
+
+mask = (trainMatrix != 0).astype(int)
+lower = simiMat * mask
+
+t4 = time()
+print('Calc lower: %.2fms' % ((t4 - t3) * 1000))
+
+predMat = upper.multiply(lower.power(-1))
+
+t5 = time()
+print('Predict: %.2fms' % ((t5 - t4) * 1000))
+
+n = testMatrix.count_nonzero()
+testMask = (testMatrix != 0).astype(int)
+maskPredMat = predMat.multiply(testMask)
+rmse = norm(maskPredMat - testMatrix) / np.sqrt(n)
+
+t6 = time()
+print('Predict test: %.2fms' % ((t6 - t5) * 1000))
 
 print('RMSE = %.2f' % rmse)
-predMat = coo_matrix((predict_data, (testMatrix.row, testMatrix.col)), shape=(hg.N, hg.N))
 with open('output/predMat.pkl', 'wb') as f:
     pkl.dump(predMat, f)
 
-t4 = time()
-print('Save result: %.2fms' % ((t4 - t3) * 1000))
+t7 = time()
+print('Save result: %.2fms' % ((t7 - t6) * 1000))
---
+++ exp2_org.py
@@ -0,0 +1,57 @@
+from scipy.sparse import csr_matrix
+from scipy.sparse import coo_matrix
+import pickle as pkl
+from time import time
+from utils import csr_cosine_similarity
+from utils import HwGlobal as hg
+import numpy as np
+
+t1 = time()
+
+with open('output/testMatrix.pkl', 'rb') as f:
+    testMatrix = pkl.load(f).tocoo()
+with open('output/trainMatrix.pkl', 'rb') as f:
+    trainMatrix = pkl.load(f).tocsc()
+with open('output/simiMat.pkl', 'rb') as f:
+    simiMat = pkl.load(f)
+
+def predict(i, j):
+    # tt1 = time()
+    traincol = trainMatrix.getcol(j)
+    upper = simiMat.getrow(i).dot(traincol)
+    # tt2 = time()
+    # 优化效果不明显，猜测原因为Python本身的缓存作用
+    lower = simiMat.getrow(i)[0, traincol.nonzero()[0]].sum()
+    # tt3 = time()
+    # print('up = %.2fms' % ((tt2 - tt1) * 1000))
+    # print('lw = %.2fms' % ((tt3 - tt2) * 1000))
+    return upper[0,0] / lower
+
+t2 = time()
+print('Load data: %.2fms' % ((t2 - t1) * 1000))
+
+rmse = 0
+predict_data = list()
+n = testMatrix.count_nonzero()
+alt = n // 100
+cnt = 0
+for (i,j,v) in zip(testMatrix.row, testMatrix.col, testMatrix.data):
+    p = predict(i, j)
+    cnt += 1
+    rmse += (p - v) ** 2
+    predict_data.append(p)
+    if cnt % alt == 0:
+        print('Process %.2f%%' % (cnt / n * 100))
+n = testMatrix.count_nonzero()
+rmse = np.sqrt(rmse / n)
+
+t3 = time()
+print('Predict test: %.2fms' % ((t3 - t2) * 1000))
+
+print('RMSE = %.2f' % rmse)
+predMat = coo_matrix((predict_data, (testMatrix.row, testMatrix.col)), shape=(hg.N, hg.N))
+with open('output/predMat.pkl', 'wb') as f:
+    pkl.dump(predMat, f)
+
+t4 = time()
+print('Save result: %.2fms' % ((t4 - t3) * 1000))
